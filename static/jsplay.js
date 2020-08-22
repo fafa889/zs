03811f36cbddd75b6114db510e15e01e2366e908
fafa889 <36075503+fafa889@users.noreply.github.com>
2020-08-22 15:26:42 +0800

    Add files via upload

---
+++ static/jsplay.js
@@ -0,0 +1,9 @@
+if (PatFrom == 'cmp' || PatFrom == 'ykyun') {
+	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/smp4.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
+} else if (PatFrom == 'migu') {
+	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/migu/?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
+} else if (PatFrom == 'ckm3u8') {
+	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/m3u8.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
+} else {
+	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/all_jx.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
+}
