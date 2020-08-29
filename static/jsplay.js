2c13421cbf3c365a9edb3a9bf18af85f284cf99b
fafa889 <36075503+fafa889@users.noreply.github.com>
2020-08-29 16:37:18 +0800

    Add files via upload

--- static/jsplay.js
+++ static/jsplay.js
@@ -1,9 +1,1 @@
-if (PatFrom == 'cmp' || PatFrom == 'ykyun') {
-	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/smp4.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
-} else if (PatFrom == 'migu') {
-	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/migu/?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
-} else if (PatFrom == 'ckm3u8') {
-	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/m3u8.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
-} else {
-	document.write('<iframe src="https://zs.jx0792.cn:1445/zs/all_jx.php?v=' + PatPlay + '"  name="video" width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>')
-}
+if(PatFrom=='cmp'||PatFrom=='ykyun'){var PatUrl='https://zs.jx0792.cn:1445/zs/smp4.php?v='}else if(PatFrom=='migu'){var PatUrl='https://zs.jx0792.cn:1445/zs/migu/?v='}else if(PatFrom=='ckm3u8'){var PatUrl='https://zs.jx0792.cn:1445/zs/m3u8.php?v='}else{var PatUrl='https://zs.jx0792.cn:1445/zs/all_jx.php?v='};document.write('<iframe src="'+PatUrl+PatPlay+'"  width="100%" height="100%" marginwidth="0" framespacing="0" marginheight="0" frameborder="0" scrolling="no" vspale="0" allowFullscreen="true"></iframe>');
