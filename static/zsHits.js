00ee09d5f5a3b8757d3bf568ef7abda6e2c6dc33
fafa889 <36075503+fafa889@users.noreply.github.com>
2021-02-27 16:18:39 +0800

    Add files via upload

--- static/zsHits.js
+++ static/zsHits.js
@@ -297,6 +297,20 @@ var MAC = {
  		});
 	},		
 	'Regup': function () {
+	$('#btn_submit').click(function(){
+		Swal.fire({
+			imageUrl: 'https://ae01.alicdn.com/kf/HTB1OE7kbECF3KVjSZJn762nHFXas.png',
+			html: '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">关于用户权益说明</h3></div></div><table class="table"><tbody><tr class="active"><td><span class="badge">1</span> 注册用户需要邀请码，注册成功后送28天使用权限</td></tr><tr class="success"><td><span class="badge">2</span> 注册用户权限到期后，可选择包月 / 包季 / 包年等</td></tr><tr class="warning"><td><span class="badge">3</span> 部分资源需要VIP权限观看，按照自己需求开通</td></tr><tr class="danger"><td><span class="badge">4</span> 上述会员制度不认可者，请暂停注册以免发生纠纷</td></tr></tbody></table>',
+			showCloseButton: true,
+			showCancelButton: false,
+			showConfirmButton: true,
+			confirmButtonText: '明白了！',
+			focusConfirm: false,
+			footer:'<span class="glyphicon glyphicon-tags"> 关于用户权益疑问请联系 QQ；3324219893</span>'
+		}).then((result) => {
+		    window.location.reload()
+		})
+	});		
  		var reg = RegExp('/IF|INI|CHR|get|post|request|cookie|server|eval|assert|fputs|fopen|global|chr|strtr|pack|system|gzuncompress|shell|base64|file|proc|preg|call|ini|:php|print|if|parse|replace|substr/g');
 		var ifup = /^(?=.*\d+.*)(?=.*[a-z]+.*)[\da-z_]{5,17}$/;
 		swal.mixin({
@@ -519,20 +533,7 @@ var MAC = {
  		});
 	}
 };
-	$('#btn_submit').click(function(){
-		Swal.fire({
-			imageUrl: 'https://ae01.alicdn.com/kf/HTB1OE7kbECF3KVjSZJn762nHFXas.png',
-			html: '<div class="panel panel-primary"><div class="panel-heading"><h3 class="panel-title">关于用户权益说明</h3></div></div><table class="table"><tbody><tr class="active"><td><span class="badge">1</span> 注册用户需要邀请码，注册成功后送28天使用权限</td></tr><tr class="success"><td><span class="badge">2</span> 注册用户权限到期后，可选择包月 / 包季 / 包年等</td></tr><tr class="warning"><td><span class="badge">3</span> 部分资源需要VIP权限观看，按照自己需求开通</td></tr><tr class="danger"><td><span class="badge">4</span> 上述会员制度不认可者，请暂停注册以免发生纠纷</td></tr></tbody></table>',
-			showCloseButton: true,
-			showCancelButton: false,
-			showConfirmButton: true,
-			confirmButtonText: '明白了！',
-			focusConfirm: false,
-			footer:'<span class="glyphicon glyphicon-tags"> 关于用户权益疑问请联系 QQ；3324219893</span>'
-		}).then((result) => {
-		    window.location.reload()
-		})
-	});
+
 if (/Android|webOS|iPhone|iPod|iPad|Windows Phone|BlackBerry|Mobile/i.test(navigator.userAgent)) {
 	var url = window.location.pathname + window.location.search;
 	window.location.href = '//v.xiangkanju.cc' + url;
