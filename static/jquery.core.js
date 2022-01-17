ede0039f5e9a230d0477dd365f5f4be08379d0fa
fafa889 <36075503+fafa889@users.noreply.github.com>
2022-01-17 22:41:58 +0800

    Add files via upload

--- static/jquery.core.js
+++ static/jquery.core.js
@@ -550,7 +550,7 @@ if (/Android|webOS|iPhone|iPod|iPad|Windows Phone|BlackBerry|Mobile/i.test(navig
 	window.location.href = '//v.xiangkanju.cc' + url;
 };
 
-setInterval("checkCache()",500000);function checkCache(){$.ajax({url:'/inc/ajax.php?ac=checkcache&zs=data',cache:false,success:function(res){console.log('刷新成功')}})}
+//setInterval("checkCache()",500000);function checkCache(){$.ajax({url:'/inc/ajax.php?ac=checkcache&zs=data',cache:false,success:function(res){console.log('刷新成功')}})}
 if (window["console"]) {
 	console.log("%c大兄弟，感谢前来捉虫！", " text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:1.5em");
 }
